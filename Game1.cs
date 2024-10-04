905561dece449fefdc4a0cfd2a2766fa13ac4679
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-04 09:13:09 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Topic_1._5___Multiple_Screens
+{
+    //Christian
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        FruitState fruitState;
+        KeyboardState keyboardState;
+        MouseState mouseState;
+
+        string message;
+
+        SpriteFont instructionFont;
+
+        Texture2D fruit1Texture;
+        Texture2D fruit2Texture;
+        Texture2D fruit3Texture;
+        Texture2D fruit4Texture;
+        Rectangle window;
+
+        enum FruitState
+        {
+            Fresh,
+            Mouldy,
+            ReallyMouldy,
+            Rotten
+        }
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+
+            fruitState = FruitState.Fresh;
+            message = "Left click the mouse to travel to the future";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            fruit1Texture = Content.Load<Texture2D>("Images/fruit1");
+            fruit2Texture = Content.Load<Texture2D>("Images/fruit2");
+            fruit3Texture = Content.Load<Texture2D>("Images/fruit3");
+            fruit4Texture = Content.Load<Texture2D>("Images/fruit4");
+            instructionFont = Content.Load<SpriteFont>("Fonts/InstructionFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+            if (fruitState == FruitState.Fresh)
+            {
+                message = "Click left mouse button to travel to the future.";
+
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    fruitState = FruitState.Mouldy;
+                }
+            }
+            else if (fruitState == FruitState.Mouldy)
+            {
+                message = "Hit spacebar to travel to the future.";
+
+                if (keyboardState.IsKeyDown(Keys.Space))
+                {
+                    fruitState = FruitState.ReallyMouldy;
+                }
+            }
+            else if (fruitState == FruitState.ReallyMouldy)
+            {
+                message = "Hit enter to travel to the future.";
+
+                if (keyboardState.IsKeyDown(Keys.Enter))
+                {
+                    fruitState = FruitState.Rotten;
+                }
+            }
+            else if (fruitState == FruitState.Rotten)
+            {
+                message = "Right click the mouse to travel to the distant past.";
+
+                if (mouseState.RightButton == ButtonState.Pressed)
+                {
+                    fruitState = FruitState.Fresh;
+                }
+            }
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            if (fruitState == FruitState.Fresh)
+            {
+                _spriteBatch.Draw(fruit1Texture, window, Color.White);
+            }
+            else if (fruitState == FruitState.Mouldy)
+            {
+                _spriteBatch.Draw(fruit2Texture, window, Color.White);
+            }
+            else if (fruitState == FruitState.ReallyMouldy)
+            {
+                _spriteBatch.Draw(fruit3Texture, window, Color.White);
+            }
+            else if (fruitState == FruitState.Rotten)
+            {
+                _spriteBatch.Draw(fruit4Texture, window, Color.White);
+            }
+
+            _spriteBatch.DrawString(instructionFont, message, new Vector2(10, 10), Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Topic_1._5___Multiple_Screens.Game1();
+game.Run();
